ff92d6e87ad62052b95d3d4b8f99c1d46d8e3c82
Lursy <115332598+Lursy@users.noreply.github.com>
2023-01-10 00:29:41 -0300

    Update search.py

--- cogs/tools/search.py
+++ cogs/tools/search.py
@@ -1,22 +1,26 @@
+import pytube
+from requests import get
+
+
 class Youtube:
     def __init__(self, video: str) -> None:
-        import pytube
-        from requests import get
         try:
             get(video)
         except:
             self.video = pytube.YouTube(pytube.Search(video).results[0].watch_url)
         else:
             self.video = pytube.YouTube(video)
-    
+
     def get_audio(self):
         return self.video.streams.get_by_itag(251).url
 
     def get_info(self):
-        return dict({
+        info = dict({
                 "Thumbnail": self.video.thumbnail_url,
                 "Titulo": self.video.title,
                 "Canal": self.video.author,
                 "Views": self.video.views,
                 "Time": self.video.length
                 })
+
+        return info
